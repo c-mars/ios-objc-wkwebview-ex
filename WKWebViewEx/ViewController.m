09e842cbd06b6c701cf5d9c12314b4ab22e43fa3
Constantine Mars <cmars@mars-book.local>
2016-04-11 19:12:02 +0300

    init

--- WKWebViewEx/ViewController.m
+++ WKWebViewEx/ViewController.m
@@ -16,12 +16,56 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    WKPreferences* preferences = [WKPreferences new];
+    preferences.javaScriptEnabled = NO;
+    
+    WKWebViewConfiguration *theConfiguration = [[WKWebViewConfiguration alloc] init];
+    theConfiguration.preferences = preferences;
+    WKWebView *webView = [[WKWebView alloc] initWithFrame:self.view.frame configuration:theConfiguration];
+    webView.navigationDelegate = self;
+    webView.UIDelegate = self;
+    
+    NSURL *nsurl = [[NSBundle mainBundle] URLForResource:@"script" withExtension:@"html"];
+    
+//    NSString* url =
+//    @"http://www.apple.com";
+//    @"http://www.w3schools.com/js/tryit.asp?filename=tryjs_alert";
+//    NSURL *nsurl=[NSURL URLWithString:url];
+    NSURLRequest *nsrequest=[NSURLRequest requestWithURL:nsurl];
+    [webView loadRequest:nsrequest];
+    
+    [self.view addSubview:webView];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+-(void)webView:(WKWebView *)webView didStartProvisionalNavigation: (WKNavigation *)navigation {
+    
+}
+
+- (void)webView:(WKWebView *)webView didFinishNavigation: (WKNavigation *)navigation{
+    
+}
+
+-(void)webView:(WKWebView *)webView didFailNavigation: (WKNavigation *)navigation withError:(NSError *)error {
+    
+}
+
+- (void)webView:(WKWebView *)webView runJavaScriptAlertPanelWithMessage:(NSString *)message initiatedByFrame:(WKFrameInfo *)frame completionHandler:(void (^)())completionHandler
+{
+    UIAlertController *alertController = [UIAlertController alertControllerWithTitle:message
+                                                                             message:nil
+                                                                      preferredStyle:UIAlertControllerStyleAlert];
+    [alertController addAction:[UIAlertAction actionWithTitle:@"OK"
+                                                        style:UIAlertActionStyleCancel
+                                                      handler:^(UIAlertAction *action) {
+                                                          completionHandler();
+                                                      }]];
+    [self presentViewController:alertController animated:YES completion:^{}];
 }
 
 @end
