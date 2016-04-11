8067ec34b5adb787dd3f001d3f40c2ca73c8dac6
Constantine Mars <konstantin.mars@ocado.com>
2016-04-11 18:38:19 +0300

    Initial Commit

---
+++ WKWebViewEx/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WKWebViewEx
+//
+//  Created by Constantine Mars on 4/11/16.
+//  Copyright © 2016 Constantine Mars. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
